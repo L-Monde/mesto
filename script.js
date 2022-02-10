99915912434523d4dc7cadff59f83758341d7164
L-Monde <dm.pshenitsyn@gmail.com>
2022-02-10 15:58:12 +0300

    feature/form script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let buttonEdit = document.querySelector('.button_edit');
-let buttonClose = document.querySelector('.button_close');
+const buttonEdit = document.querySelector('.button_edit');
+const buttonClose = document.querySelector('.button_close');
+const buttonSubmit = document.querySelector('.form__submit');
 
 function popupHide() {
     document.querySelector('.popup').classList.remove('popup_opened');
@@ -9,5 +10,15 @@ function popupDisplay() {
     document.querySelector('.popup').classList.add('popup_opened');
 }
 
+function profileUpdate() {
+    const formName = document.querySelector('.form__edit_name').value;
+    const formDesc = document.querySelector('.form__edit_desc').value;
+    document.querySelector('.profile__name').innerHTML = formName;
+    document.querySelector('.profile__description').innerHTML = formDesc;
+    document.querySelector('.popup').classList.remove('popup_opened');
+}
+
+
 buttonEdit.addEventListener('click', popupDisplay);
 buttonClose.addEventListener('click', popupHide);
+buttonSubmit.addEventListener('click', profileUpdate);
