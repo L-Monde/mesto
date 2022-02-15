6defd15ea725deba63521b537d8445f525072931
L-Monde <dm.pshenitsyn@gmail.com>
2022-02-15 03:21:23 +0300

    Попытка #4

--- scripts/script.js
+++ scripts/script.js
@@ -3,30 +3,32 @@ const buttonClose = document.querySelector('.popup__button-close');
 const form = document.querySelector('.popup__form');
 const popup = document.querySelector('.popup');
 const profileName = document.querySelector('.profile__name');
-const profilrDescription = document.querySelector('.profile__description');
+const profileDescription = document.querySelector('.profile__description');
+const formName = document.querySelector('.popup__form_name');
+const formDesc = document.querySelector('.popup__form_description');
 
 function popupHide() {
     popup.classList.remove('popup_opened');
 }
 
+function copy() {
+    formName.value = profileName.textContent;
+    formDesc.value = profileDescription.textContent;
+}
+
 function popupDisplay() {
+    copy();
     popup.classList.add('popup_opened');
 }
 
 function profileUpdate() {
-    const formName = document.querySelector('.popup__edit-name').value;
-    const formDesc = document.querySelector('.popup__edit-desc').value;
-    profileName.textContent = formName;
-    profilrDescription.textContent = formDesc;
+    event.preventDefault();
+    profileName.textContent = formName.value;
+    profileDescription.textContent = formDesc.value;
     popupHide();
 }
 
-
-
 buttonEdit.addEventListener('click', popupDisplay);
-buttonClose.addEventListener('click', () => {
-    popupHide();
-});
+buttonClose.addEventListener('click', popupHide);
 
-function handleForm(event) { event.preventDefault(); }
 form.addEventListener('submit', profileUpdate);
